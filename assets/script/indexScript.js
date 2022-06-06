9b01a7ff2d0952a601296b7a381dbe9b6b4eca74
Flavio Jeronimo da Silva Filho <flaviofilho.js@gmail.com>
2022-06-06 18:34:47 -0300

    finalizado parte inicial de escolha do monster

--- assets/script/indexScript.js
+++ assets/script/indexScript.js
@@ -21,7 +21,7 @@ function arrowRight() {
     document.querySelector(".name-monster").style.setProperty("opacity", "50%");
     document.querySelector(".name-monster").innerHTML = "";
     document.querySelector(".main-info").innerHTML = //como jogar
-      "<ul><li>Escolha seu ScriptMonster</li><li>Cada ScriptMonster tem 3 habilidades <br /><ul><li>Mordida (ataque neutro)</li><li>Skill (ataque elemental)</li><li>Curar (cura % de HP)</li></ul></li><li>Durante o jogo escolha uma das 3 ações</li></ul>";
+      "<ul><li>Escolha seu ScriptMonster</li><li>Cada ScriptMonster tem 3 habilidades <br /><ul><li>Mordida (ataque neutro)</li><li>Skill (ataque elemental)</li><li>Curar (cura HP)</li><li>Esquivar (aumenta esquiva 50%)</li></ul></li><li>Durante o jogo escolha uma das 4 ações</li></ul>";
     document.querySelector(".h2-container").innerHTML = "<h2>Como Jogar</h2>";
     document
       .querySelector(".monsters-img")
@@ -107,7 +107,7 @@ function arrowLeft() {
     document.querySelector(".name-monster").style.setProperty("opacity", "50%");
     document.querySelector(".name-monster").innerHTML = "";
     document.querySelector(".main-info").innerHTML = //como jogar
-      "<ul><li>Escolha seu ScriptMonster</li><li>Cada ScriptMonster tem 3 habilidades <br /><ul><li>Mordida (ataque neutro)</li><li>Skill (ataque elemental)</li><li>Curar (cura % de HP)</li></ul></li><li>Durante o jogo escolha uma das 3 ações</li></ul>";
+      "<ul><li>Escolha seu ScriptMonster</li><li>Cada ScriptMonster tem 4 habilidades <br /><ul><li>Mordida (ataque neutro)</li><li>Skill (ataque elemental)</li><li>Curar (cura HP)</li><li>Esquivar (aumenta esquiva 50%)</li></ul></li><li>Durante o jogo escolha uma das 4 ações</li></ul>";
     document.querySelector(".h2-container").innerHTML = "<h2>Como Jogar</h2>";
     document
       .querySelector(".monsters-img")
@@ -173,4 +173,132 @@ function arrowLeft() {
         "url(./assets/img/IceMonster/Ice-Monster-normal.png)"
       );
   }
+}
+
+//Definindo monster do Player
+
+let monsterID = 0;
+
+let fireMonsterStatus = {
+  ID: 1,
+  HP: 2000,
+  SkillAtk: 200,
+  NormalAtk: 100,
+  Cura: 80,
+  EsquivaChance: 10,
+  CritChance: 50,
+};
+
+let aquaMonsterStatus = {
+  ID: 2,
+  HP: 2000,
+  SkillAtk: 200,
+  NormalAtk: 100,
+  Cura: 100,
+  EsquivaChance: 10,
+  CritChance: 40,
+};
+
+let iceMonsterStatus = {
+  ID: 3,
+  HP: 2000,
+  SkillAtk: 200,
+  NormalAtk: 100,
+  Cura: 80,
+  EsquivaChance: 15,
+  CritChance: 40,
+};
+
+var player1 = {
+  ID: 0,
+  HP: 0,
+  SkillAtk: 0,
+  NormalAtk: 0,
+  Cura: 0,
+  EsquivaChance: 0,
+  CritChance: 0,
+};
+var player2 = {
+  ID: 0,
+  HP: 0,
+  SkillAtk: 0,
+  NormalAtk: 0,
+  Cura: 0,
+  EsquivaChance: 0,
+  CritChance: 0,
+};
+
+function escolherMonster1() {
+  monsterID = maxArrowClicks;
+  if (monsterID == 0) {
+    alert("Escolha um script monster");
+    return 0;
+  }
+  if (monsterID == fireMonsterStatus.ID) {
+    player1 = { fireMonsterStatus };
+    maxArrowClicks = 0;
+    document.querySelector(".arrow.left").style.setProperty("opacity", "50%");
+    arrowLeft();
+  } else if (monsterID == aquaMonsterStatus.ID) {
+    maxArrowClicks = 0;
+    player1 = { aquaMonsterStatus };
+    document.querySelector(".arrow.left").style.setProperty("opacity", "50%");
+    arrowLeft();
+  } else if (monsterID == iceMonsterStatus.ID) {
+    maxArrowClicks = 0;
+    player1 = { iceMonsterStatus };
+    document.querySelector(".arrow.left").style.setProperty("opacity", "50%");
+    arrowLeft();
+  }
+
+  alert(
+    `Monster Escolhido com sucesso ! Antes do Player2 Escolher o monster, clique em "OK" para que ele não veja o Monster esolhido`
+  );
+
+  document
+    .querySelector(".action-button.player1")
+    .style.setProperty("display", "none");
+
+  document
+    .querySelector(".action-button.player2")
+    .style.setProperty("display", "inline");
+}
+
+function escolherMonster2() {
+  monsterID = maxArrowClicks;
+  if (monsterID == 0) {
+    alert("Escolha um script monster");
+    return 0;
+  }
+  if (monsterID == fireMonsterStatus.ID) {
+    player2 = { fireMonsterStatus };
+    maxArrowClicks = 0;
+    document.querySelector(".arrow.left").style.setProperty("opacity", "50%");
+    arrowLeft();
+  } else if (monsterID == aquaMonsterStatus.ID) {
+    maxArrowClicks = 0;
+    player2 = { aquaMonsterStatus };
+    document.querySelector(".arrow.left").style.setProperty("opacity", "50%");
+    arrowLeft();
+  } else if (monsterID == iceMonsterStatus.ID) {
+    maxArrowClicks = 0;
+    player2 = { iceMonsterStatus };
+    document.querySelector(".arrow.left").style.setProperty("opacity", "50%");
+    arrowLeft();
+  }
+
+  alert(`Monster Escolhido com sucesso ! Que vença o melhor ScriptMaster`);
+
+  document
+    .querySelector(".action-button.player1")
+    .style.setProperty("display", "none");
+
+  document
+    .querySelector(".action-button.player2")
+    .style.setProperty("display", "none");
+
+  document.querySelector(".player1View").innerHTML = "";
+  document.querySelector(".player2View").innerHTML = "";
+
+  //parei na parte de criar o layout de batalha
 }
